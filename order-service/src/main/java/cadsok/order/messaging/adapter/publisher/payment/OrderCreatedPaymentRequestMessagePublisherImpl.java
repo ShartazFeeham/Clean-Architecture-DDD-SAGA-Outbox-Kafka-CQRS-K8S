8109187e4fca05a275341b6da14d4404b572cba7
ShartazFeeham <mdfeeham@gmail.com>
2025-11-01 01:07:05 +0600

    new message publisher implemented in order service, synced existing one

--- order-service/src/main/java/cadsok/order/messaging/adapter/publisher/payment/OrderCreatedPaymentRequestMessagePublisherImpl.java
+++ order-service/src/main/java/cadsok/order/messaging/adapter/publisher/payment/OrderCreatedPaymentRequestMessagePublisherImpl.java
@@ -2,12 +2,12 @@ package cadsok.order.messaging.adapter.publisher.payment;
 
 import cadsok.order.domain.application.ports.output.message.publisher.payment.OrderCreatedPaymentRequestMessagePublisher;
 import cadsok.order.domain.core.event.OrderCreatedEvent;
-import cadsok.order.messaging.service.OrderEventProducerService;
+import cadsok.order.messaging.service.OrderCreatedEventPublisher;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Component;
 
 @Component
-public class OrderCreatedPaymentRequestMessagePublisherImpl extends OrderEventProducerService<OrderCreatedEvent>
+public class OrderCreatedPaymentRequestMessagePublisherImpl extends OrderCreatedEventPublisher<OrderCreatedEvent>
         implements OrderCreatedPaymentRequestMessagePublisher {
     public OrderCreatedPaymentRequestMessagePublisherImpl(KafkaTemplate<String, String> kafkaTemplate) {
         super(kafkaTemplate);
